b120f8cc79684d8ac83f720bbd447b26dec21a94
Richard Kohl <zeisigdm@gmail.com>
2022-09-23 11:17:26 +0200

    Klassen Einführungz

--- Program.cs
+++ Program.cs
@@ -16,8 +16,12 @@ namespace Einführung_Klassen
             //student1.Vorname = "Khalil";
 
             Student student1 = new Student("Rohan", "Acharya", 20, "Nepal", "T21", "27.11.2001");
-            
+            student1.Begrüsung();
+            Student student2 = new Student("Khalil", "Acharya", 20, "Nepal", "T21", "27.11.2001");
+            student2.Begrüsung();
 
+            Hund hund1 = new Hund("Firulais", "Schwarz", 7, 75);
+            hund1.Bellen();
         }
     }
     /* --- Klassen in C#---
@@ -70,7 +74,41 @@ namespace Einführung_Klassen
         private static void Geburtstag()
         {
             //Wenn der Student Geburtstag hat, wird sein Alter um 1 erhöht.
-            DateTime
+            DateTime dt = DateTime.Now; //Die aktuelle Uhrzeit und das aktuelle Datum
+            Console.WriteLine(dt.Date);
+        }
+        public void Begrüsung()
+        {
+            Console.WriteLine(Vorname + " sagt Halle!");
+        }
+        
+    }
+    /*Übung 1: Erstellen Sie die Klasse Hund. Onjekte in dieser Klasse haben
+     * die Eigenschaften:
+     * Farbe; Größe, Name, Alter.
+     * In der Klasse gibt es die Methode Bellen(). Wenn die Methode in der
+     * Main-Methode aufgerufen wird, wird "Name bellt." in die Konsole
+     * geschrieben. Erstellen Sie einen Hund in der Main-Methode und lassen Sie
+     * ihn bellen.
+     */
+    class Hund
+    {
+
+        public string Name;
+        public string Farbe;
+        public int Alter;
+        public double Größe;
+        public Hund(string name, string farbe, int alter, double größe)
+        {
+            Name = name;
+            Farbe = farbe;
+            Alter = alter;
+            Größe = größe;
+
+        }
+        public void Bellen()
+        {
+            Console.WriteLine(Name + " bellt jetzt. " + Name +" blafft Sie an");
         }
     }
 }
