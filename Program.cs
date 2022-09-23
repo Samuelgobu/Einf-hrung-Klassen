416d29e4842f4315e67851d057cfc16c54c2e505
Richard Kohl <zeisigdm@gmail.com>
2022-09-23 10:40:38 +0200

    klasse einführung

--- Program.cs
+++ Program.cs
@@ -6,10 +6,64 @@ using System.Threading.Tasks;
 
 namespace Einführung_Klassen
 {
-    class Program
+
+    internal class Program
     {
         static void Main(string[] args)
         {
+            //Student student1 = new Student();
+            //student1.Vorname = "Rohan";
+            //student1.Vorname = "Khalil";
+
+            Student student1 = new Student("Rohan", "Acharya", 20, "Nepal", "T21");
+            
+
+        }
+    }
+    /* --- Klassen in C#---
+     * Klassen ermöglichen es Code in C# weiter zu strukturieren und
+     * übersichtlicher zu machen
+     * 
+     * Man unercheidet zwischen statischen Klassen ohne Objekte und 
+     * Klassen mit Objekten.
+     * 
+     * Wir können zum Beispiel eine Klasse erstellen, in der Studenten 
+     * gespeichert sind. Die Studenten sind in dem Fall die Objekte in der Klasse.
+     * 
+     * ---Eigenschaften---
+     * Jedes Objekt benötigt Eigenschaften, damit man zwischen mehreren Objekten
+     * unterscheiden kann. Eine Eigenschaft ist eine besondere Variable.
+     * Eigenschaften sind GEKAPSELTE Variablen. Das bedeutet: nicht jedes Objekt
+     * oder jede Methode kann ohne Weiteres auf diese Variablen zugriefen.
+     * 
+     * Ein Student hat zum Beispiel folgende Eigenschaften:
+     * Vorname, Nachname, Alter, Nationalität, Gruppe, ...
+     */
+    class Student
+    {
+        // Am Anfang der Klasse werden die Eigenschaften definiert:
+        public string Vorname { get; set; }
+        private string Nachname;
+        private int Alter;
+        private string Nationalität;
+        private string Gruppe;
+        /*Eigenschaften können public oder private sein. Eine public
+         * Eigenschaft kann auch in anderen Klassen gesehen verändert
+         * werden. Eine private Eigenschaft ist nur Methoden in der eigenen
+         * Klasse sichtbar. Normalerweise sollten Eigenschaften private sein.
+         * 
+         * Wenn Eigenschaften privat sind, braucht man einen KONSTRUKTOR
+         * um Objekte mir dieser Eigenschaft zu erzeugen.
+         * Ein Konstruktor sieht zum Beispiel so aus:
+         * 
+         */
+        public Student(string vorname, string nachname, int alter, string nation, string gruppe)
+        {
+            Vorname = vorname;
+            Nachname = nachname;
+            Alter = alter;
+            Nationalität = nation;
+            Gruppe = gruppe;
         }
     }
 }
